b46fad2e54e7be282274621960131be9e3339b90
kpilszak <klaudia.pilszak@gmail.com>
2020-05-19 12:14:42 +0200

    Create repo

---
+++ src/main/java/com/kpilszak/conferenceregistration/ConferenceRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.kpilszak.conferenceregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpilszak/conferenceregistration/ConferenceRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpilszak.conferenceregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
